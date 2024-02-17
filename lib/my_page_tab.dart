9651f9622f894f2e28d0ca37daacef74d8596a02
Haruki1090 <haruharu.com.109607@gmail.com>
2024-02-17 15:02:29 +0900

    userImage, bookCountの追加

--- lib/my_page_tab.dart
+++ lib/my_page_tab.dart
@@ -1,3 +1,4 @@
+import 'package:books_rater/editing_my_data.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -7,6 +8,7 @@ class MyPageTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     final user = FirebaseAuth.instance.currentUser;
 
     return Scaffold(
@@ -29,10 +31,29 @@ class MyPageTab extends StatelessWidget {
           }
 
           final userData = snapshot.data?.data() as Map<String, dynamic>;
-          final username = userData['username'] ?? '不明なユーザー';
+          final userImage = userData['imageUrl'] ?? 'https://www.shoshinsha-design.com/wp-content/uploads/2020/05/noimage-760x460.png';
+          final username = userData['username'] ?? 'ゲスト';
+          final bookCount = userData['bookCount'] ?? '取得中';
 
-          return Center(
-            child: Text('こんにちは、$usernameさん', style: TextStyle(fontSize: 24)),
+          return Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(90, 20, 90, 20),
+                child: Image.network(userImage),
+              ),
+              const SizedBox(height: 16),
+              Text('$username', style: TextStyle(fontSize: 24)),
+              const SizedBox(height: 16),
+              TextButton(onPressed: (){
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const EdditingUserDataPage()
+                    )
+                );
+              }, child: const Text('プロフィールを編集')),
+              const SizedBox(height: 16),
+              Text('登録した本：$bookCount冊', style: TextStyle(fontSize: 20)),
+            ],
           );
         },
       ) : const Center(child: Text('ログインしていません')),
