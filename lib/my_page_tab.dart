9f36176208112ce6a1fdd85000cb1200718d6002
Haruki1090 <haruharu.com.109607@gmail.com>
2024-02-17 14:23:26 +0900

    Firebase Auth の currentUser から現在ログイン中のアカウントを判定。 この情報をもとに、e-mailを辿ってFirebase Firestoreにアクセスし、必要なデータを取得する。(username)

--- lib/my_page_tab.dart
+++ lib/my_page_tab.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class MyPageTab extends ConsumerWidget {
+class MyPageTab extends StatelessWidget {
   const MyPageTab({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return const Center(
-      child: Text('MyPageTab', textAlign: TextAlign.center, style: TextStyle(fontSize: 30)),
+  Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('マイページ'),
+      ),
+      body: user != null ? FutureBuilder<DocumentSnapshot>(
+        future: FirebaseFirestore.instance.collection('users').doc(user.email).get(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return Center(child: Text('エラーが発生しました: ${snapshot.error}'));
+          }
+
+          if (!snapshot.hasData) {
+            return const Center(child: Text('ユーザーデータが見つかりません'));
+          }
+
+          final userData = snapshot.data?.data() as Map<String, dynamic>;
+          final username = userData['username'] ?? '不明なユーザー';
+
+          return Center(
+            child: Text('こんにちは、$usernameさん', style: TextStyle(fontSize: 24)),
+          );
+        },
+      ) : const Center(child: Text('ログインしていません')),
     );
   }
 }
