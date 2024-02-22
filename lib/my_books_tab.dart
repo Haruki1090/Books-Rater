97a976a97c7ea265f0cba931c37464f90888d325
Haruki1090 <haruharu.com.109607@gmail.com>
2024-02-22 13:33:19 +0900

    削除

--- lib/my_books_tab.dart
+++ lib/my_books_tab.dart
@@ -11,8 +11,7 @@ class MyBooksTab extends ConsumerStatefulWidget {
 class _MyBooksTabState extends ConsumerState<MyBooksTab> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Text('MyBooksTab', textAlign: TextAlign.center, style: TextStyle(fontSize: 30))
+    return Scaffold(
     );
   }
 }
