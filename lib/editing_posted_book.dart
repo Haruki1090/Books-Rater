1a17ee31e0a37ad783e10277bce23d2208167d14
Haruki1090 <haruharu.com.109607@gmail.com>
2024-02-27 18:04:58 +0900

    不使用ファイルを削除

--- lib/editing_posted_book.dart
+++ lib/editing_posted_book.dart
@@ -1,4 +1,3 @@
-import 'package:books_rater/posting_new_book.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
