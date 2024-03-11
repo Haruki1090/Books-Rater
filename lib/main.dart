e6c018ecb58a0daf1458a5f3c74242515d5909c9
Haruki1090 <haruharu.com.109607@gmail.com>
2024-03-11 16:47:37 +0900

    デバイスの向きによらず画面固定（常に縦画面で固定）

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,18 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'firebase_options.dart';
+import 'package:flutter/services.dart';
 
 Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(
-      const ProviderScope(child: MyApp())
-  );
+  SystemChrome.setPreferredOrientations([
+    // 縦画面固定
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((_) => runApp(const ProviderScope(child: MyApp())));
 }
 
 class MyApp extends ConsumerWidget {
