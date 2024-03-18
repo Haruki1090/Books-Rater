5e5a459dff2d732c07932784b43b5fe41234d07d
Haruki1090 <haruharu.com.109607@gmail.com>
2024-03-18 16:26:16 +0900

    ログイン保持OK

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:books_rater/home.dart';
+import 'package:books_rater/home_page_tab.dart';
 import 'package:books_rater/setting_page.dart';
 import 'package:books_rater/sign_in_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -94,7 +97,18 @@ class MyApp extends ConsumerWidget {
         shadowColor: Colors.white24, // ダークモードでの影の色
       ),
       themeMode: themeMode,
-      home: const SignInPage(),
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          }
+          if (snapshot.hasData) {
+            return const Home();
+          }
+          return const SignInPage();
+        },
+      ),
     );
   }
 }
